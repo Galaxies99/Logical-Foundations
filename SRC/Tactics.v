d45dafc90fb05e1c4d38777618ac9ec5ea6345be
galaxies99 <galaxies@vip.qq.com>
2019-07-22 13:31:22 +0800

    Tactics

--- SRC/Tactics.v
+++ SRC/Tactics.v
@@ -420,8 +420,10 @@ Qed.
   intros: move hypotheses/variables from goal to context
   reflexivity: finish the proof (when the goal looks like e = e)
   apply: prove goal using a hypothesis, lemma, or constructor
-  apply... in H: apply a hypothesis, lemma, or constructor to a hypothesis in the context (forward reasoning)
-  apply... with...: explicitly specify values for variables that cannot be determined by pattern matching
+  apply... in H: apply a hypothesis, lemma, or constructor to a hypothesis in the context
+                (forward reasoning)
+  apply... with...: explicitly specify values for variables that cannot be determined by 
+                    pattern matching
   simpl: simplify computations in the goal
   simpl in H: ... or a hypothesis
   rewrite: use an equality hypothesis (or lemma) to rewrite the goal
@@ -431,12 +433,15 @@ Qed.
   unfold: replace a defined constant by its right-hand side in the goal
   unfold... in H: ... or a hypothesis
   destruct... as...: case analysis on values of inductively defined types
-  destruct... eqn:...: specify the name of an equation to be added to the context, recording the result of the case analysis
+  destruct... eqn:...: specify the name of an equation to be added to the context, 
+                       recording the result of the case analysis
   induction... as...: induction on values of inductively defined types
   injection: reason by injectivity on equalities between values of inductively defined types
-  discriminate: reason by disjointness of constructors on equalities between values of inductively defined types
+  discriminate: reason by disjointness of constructors on equalities between values of 
+                inductively defined types
   assert (H: e) (or assert (e) as H): introduce a "local lemma" e and call it H
-  generalize dependent x: move the variable x (and anything else that depends on it) from the context back to an explicit hypothesis in the goal formula
+  generalize dependent x: move the variable x (and anything else that depends on it) from 
+                          the context back to an explicit hypothesis in the goal formula
 *)
 
 (* Chap 5.9 Additional Exercises *)
